992b44dd5430c43eae1ebfa2992503e8f4cd17be
TAICHI <taichi@TAICHI-no-MacBook-Air.local>
2014-09-02 19:57:31 +0900

    Initial Commit

---
+++ AlermClock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlermClock
+//
+//  Created by TAICHI on 2014/09/02.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
